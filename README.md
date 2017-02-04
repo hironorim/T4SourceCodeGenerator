3180f928edb496ce810d47b97d39c3daeaae4a12
hironori <hironori.m@gmail.com>
2017-02-04 14:06:27 +0900

    サンプル作成

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# T4TemplateSample
+# TextTransform ユーティリティを使用したファイルの生成サンプル
+[参考ページ](https://msdn.microsoft.com/ja-jp/library/bb126245.aspx)
+
+## 付属するサンプル
+* sample1  
+csvからSQL文を生成する
+* sample2  
+コマンドラインパラメータから値を渡す
+* sample3  
+JSONデータからhtmlを生成する
+
+## 使用方法
+1. Visual Studioをインストール
+2. 本リポジトリをclone
+3. allSample.ps1を実行するとすべてのサンプルが実行される
--- allSample.ps1
+++ allSample.ps1
@@ -1,4 +1,5 @@
 Set-Item Env:Path (Get-ChildItem "C:\Program Files (x86)\Common Files\Microsoft Shared\TextTemplating" | Select-Object -Last 1).FullName
 
-TextTransform.exe sample1.tt -o sample1.txt -a !!DataFile!sample1.tsv
+TextTransform.exe sample1.tt -o sample1.sql -a !!DataFile!sample1.csv
 TextTransform.exe sample2.tt -o sample2.txt -a !!name!Ichiro
+TextTransform.exe sample3.tt -o sample3.html -a !!JsonFile!sample3.json
